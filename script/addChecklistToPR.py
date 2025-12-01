cce86cca402a49575fb63e35697b4d2dd09cee21
Dinesh Senthilvel <dinesh.senthilvel@in.bosch.com>
2025-12-01 20:21:18 +0530

    Update addChecklistToPR.py

--- script/addChecklistToPR.py
+++ script/addChecklistToPR.py
@@ -78,7 +78,10 @@ def main(pr_title: str, repo: str, pr_number: int, file_base_url: str, token: st
         # Step 1 — Get the latest commit affecting this file
         commits_url = f"https://api.github.com/repos/{chkowner}/{chkrepo}/commits"
         params = {"path": FILEPATH, "per_page": 1}
-        headers = {"Authorization": f"token {token}"}
+        headers = {
+            'Authorization': f'token {token}',
+            'Content-Type': 'application/json'
+        }
         print(commits_url)
         print(params)
         commit_resp = requests.get(commits_url, params=params, headers=headers)
