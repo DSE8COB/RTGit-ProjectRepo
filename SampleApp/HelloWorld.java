1c8190814e5e9ec0fdf06ce1b1641a886701fec9
Dinesh Senthilvel <dinesh.senthilvel@in.bosch.com>
2026-06-17 18:53:36 +0530

    Update HelloWorld.java

--- SampleApp/HelloWorld.java
+++ SampleApp/HelloWorld.java
@@ -5,7 +5,7 @@
  *  Prints "Hello, World". By tradition, this is everyone's first program.
  *
  *  % java HelloWorld
- *  Hello, World sfds
+ *  Hello, World
  *
  *  These 17 lines of text are comments. They are not part of the program;
  *  they serve to remind us about its properties. The first two lines tell
